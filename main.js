e7973a009dcb163065e4aba6a4a315db2a9bbbb5
Vedanta Somnathe <vedanta1412@gmail.com>
2022-07-31 16:26:20 +0530

    call

---
+++ main.js
@@ -0,0 +1,43 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 4000;
+const cookieParser = require("cookie-parser");
+const server = require("http").Server(app);
+const socket = require("socket.io");
+const io = socket(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+const cors = require("cors");
+
+// SETTINGS
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(__dirname + "/public"));
+app.use(cors());
+
+// IO
+io.on("connection", (socket) => {
+    socket.on("connected", (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).emit("new-user", userId);
+    });
+
+    socket.on("disconnect-user", (roomId, userId) => {
+        socket.to(roomId).emit("disconnect-user", userId);
+    });
+});
+
+// ROUTES
+app.get("/:room", (req, res) => {
+    res.json({ roomId: req.params.room });
+});
+
+server.listen(PORT, () => console.log(`server is running on port ${PORT}`));
